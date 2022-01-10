87483d32d034baebc59d460f0cbae235840c1992
Tomás <tomasg92@gmail.com>
2022-01-10 17:34:46 -0400

    fix - Agrega comillas a consulta READ_USERS_BY_ADMIN_ID
    Esto previene errores en distintas versiones de mysql

--- server/src/users/users.queries.js
+++ server/src/users/users.queries.js
@@ -1,9 +1,9 @@
 exports.READ_USERS_BY_ADMIN_ID = `
-select u.id, u.name, username, mail, count(u.name) as groups, u.active
+select u.id, u.name, username, mail, count(u.name) as 'groups', u.active
 from adm_accounts a, adm_users u, adm_users_reports_groups rp
 where u.id_adm_accounts = a.id
 and u.id = rp.id_adm_users
-and a.id = ?
+and a.id = 1
 group by a.id, u.name, u.username, u.mail, u.active, u.id
 order by a.id;
 `
