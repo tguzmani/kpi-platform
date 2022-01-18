126cd019a208eb618ab97319309b74f55f8c7cb6
Tomás <tomasg92@gmail.com>
2022-01-18 03:45:10 -0400

    Actualiza sintaxis de kpi-platform.sh

--- kpi-platform.sh
+++ kpi-platform.sh
@@ -5,30 +5,41 @@ case $1 in
   -b)
     cd $kpi_dir/client
     bash build.sh
+    ;;
+
+  -d)
+    cd $kpi_dir
+    ;;
 
   -p)
     cd $kpi_dir
     git pull
+    ;;
 
   -r)
     cd $kpi_dir/server
     screen -s server npm run server
+    ;;
 
   -t)
     screen -X -S server quit
+    ;;
 
   -w)
     screen -r server
-    
+    ;;
+
   -h)
     echo -b \t build frontend
+    echo -d \t goto directory
     echo -p \t make git pull request
     echo -r \t run server
     echo -t \t stop server
     echo -w \t show server status
+    ;;
   
   *)
-    USAGE: kpi-platform [OPTION]
-    Options available via kpi-platform -h
-
-  
+    echo USAGE: kpi-platform [OPTION]
+    echo Options available via kpi-platform -h
+    ;;
+esac
