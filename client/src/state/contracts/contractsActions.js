f40b21d5e3d94a9a7964ef276fcc9d4a19f3b7a9
Tomás <tomasg92@gmail.com>
2022-01-27 22:17:49 -0400

    migracion

--- client/src/state/contracts/contractsActions.js
+++ client/src/state/contracts/contractsActions.js
@@ -27,7 +27,7 @@ export const readContractByAdmin = () => async dispatch => {
   setLoading()(dispatch)
 
   try {
-    const res = await axios.get(`/contracts/`)
+    const res = await axios.get(`/contracts`)
     dispatch({ type: READ_CONTRACT_BY_ADMIN, payload: res.data })
   } catch (error) {
     dispatch({ type: ERROR, payload: error.response.data.message })
